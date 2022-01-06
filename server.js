7bd3408c7a653c47991a645762b4a8a284a6d768
bonobo <arburpee@gmail.com>
2022-01-05 19:48:30 -0800

    added query statements and server connection

--- server.js
+++ server.js
@@ -17,9 +17,45 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 )
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
 });
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, rows) => {
+//     if (err) {
+//         console.log(err);
+//     };
+//     console.log(rows);
+// });
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//              VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 app.use((req, res) => {
     res.status(404).end();
